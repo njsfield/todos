ed3388ed90e466616bff6e56342c3ac272b3308c
Nick Field <heyimnick@live.com>
2017-01-13 12:26:44 +0000

    add local storage support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import persistState from 'redux-localstorage';
 import todoApp from './reducers';
 import App from './components/app';
 
-let store = createStore(todoApp);
+let enhancer = compose(
+  persistState(null, {key: 'todostorage'})
+);
+
+let store = createStore(todoApp, enhancer);
 
 render(
   <Provider store={store}>
