5aa427c1eba3e9256937ff1363c0e7f28f7e500e
Nick Field <heyimnick@live.com>
2017-01-13 15:50:14 +0000

    refactor wordToColour to functional style

--- src/helpers/wordtocolour.js
+++ src/helpers/wordtocolour.js
@@ -1,17 +1,27 @@
-// Needs Refactor
-const wordToColour = (text) => {
-  let colourMap = {
-    css: 'blue',
-    javascript: 'red',
-    databases: 'mid-gray',
-    github: 'green',
-    email: 'orange',
-    design: 'pink'
-  };
-  let colour = 'black';
-  text.split(" ").forEach(word =>
-    colourMap[word.toLowerCase()] ? colour = colourMap[word.toLowerCase()] : '');
-  return colour;
+// Utilities
+const compose = (a, b, x) => a(b(x));
+
+const lower = (text) => text.toLowerCase();
+
+const splitString = (text) => text.split(" ");
+
+const findVal = (key, obj) => obj[key];
+
+const lowerWordArray = (text) => compose(splitString, lower, text);
+
+const catchKey = (obj, text) => lowerWordArray(text).find(word => findVal(word, obj));
+
+// Word/Colour Map
+const wordColours = {
+  css: 'blue',
+  javascript: 'red',
+  databases: 'mid-gray',
+  github: 'green',
+  email: 'orange',
+  design: 'pink'
 };
+
+// Output
+const wordToColour = (text) => wordColours[catchKey(wordColours, text)] || 'black';
 
 export default wordToColour;
